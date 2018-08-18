d300917ee7c6d9a7b8ae7fb83acbaf8f2a2a0a9d
raygu0101 <raygu0101@gmail.com>
2018-08-18 16:48:12 +0800

    [+]framework

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 // 加载 composer
 require_once __DIR__.'/../vendor/autoload.php';
 
+// 加载 .env 文件
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
@@ -11,12 +12,15 @@ try {
 
 /*
 |--------------------------------------------------------------------------
-| Create The Application
+| Create The Application 创建应用
 |--------------------------------------------------------------------------
 |
 | Here we will load the environment and create the application instance
 | that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
+|
+| 加载环境设置，并创建应用实例作为框架中心
+| 我们将使用这个应用作为“控制反转”（IoC）容器，并且路由这个框架
 |
 */
 
@@ -31,11 +35,15 @@ $app = new Laravel\Lumen\Application(
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
+| 注册容器绑定
 |--------------------------------------------------------------------------
 |
 | Now we will register a few bindings in the service container. We will
 | register the exception handler and the console kernel. You may add
 | your own bindings here if you like or you can make another file.
+|
+| 这里我们在这里注册了一些绑定。包括异常处理和控制器核心。
+| 你可以在这里添加你自己的绑定，或者创建一个文件。
 |
 */
 
@@ -52,11 +60,16 @@ $app->singleton(
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
+| 注册中间件
 |--------------------------------------------------------------------------
 |
 | Next, we will register the middleware with the application. These can
 | be global middleware that run before and after each request into a
 | route or middleware that'll be assigned to some specific routes.
+|
+| 这里我们可以注册应用的中间件
+| 1、全局中间件，在特定的事件执行
+| 2、路由中间件，在进入之前和退出 route 时执行
 |
 */
 
@@ -71,11 +84,15 @@ $app->singleton(
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
+| 注册服务提供者
 |--------------------------------------------------------------------------
 |
 | Here we will register all of the application's service providers which
 | are used to bind services into the container. Service providers are
 | totally optional, so you are not required to uncomment this line.
+|
+| 这里我们将注册应用所有的服务提供者
+| 服务提供者是可选参数，所以你不一定要取消下面的注释
 |
 */
 
@@ -86,11 +103,15 @@ $app->singleton(
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
+| 加载应用路由
 |--------------------------------------------------------------------------
 |
 | Next we will include the routes file so that they can all be added to
 | the application. This will provide all of the URLs the application
 | can respond to, as well as the controllers that may handle them.
+|
+| 加载所有路由文件，将路由配置加载到应用中。
+| 这里会提供应用所有会响应的 URL，并且定义处理的 controllers。
 |
 */
 
